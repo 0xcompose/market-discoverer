e09dfab208993b7448b3e63bedb8717ada15365b
0xcompose <0xcompose@maat.finance>
2025-11-15 23:47:34 +0300

    refactor(config): pull config from toml file

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,49 @@
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
+
+use serde::Deserialize;
+use toml;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Config {
+    pub name: String,
+    pub data_endpoint_url: String,
+    pub cache_file_path: String,
+}
+
 #[derive(Debug)]
-pub struct Config<'a> {
-    pub name: &'a str,
-    pub data_endpoint_url: &'a str,
-    pub cache_file_path: &'a str,
+pub enum ConfigReadError {
+    Io(std::io::Error),
+    Toml(toml::de::Error),
+}
+
+impl Display for ConfigReadError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            ConfigReadError::Io(e) => write!(f, "IO error: {}", e),
+            ConfigReadError::Toml(e) => write!(f, "Toml error: {}", e),
+        }
+    }
+}
+
+impl Error for ConfigReadError {}
+
+impl From<std::io::Error> for ConfigReadError {
+    fn from(err: std::io::Error) -> Self {
+        ConfigReadError::Io(err)
+    }
+}
+
+impl From<toml::de::Error> for ConfigReadError {
+    fn from(err: toml::de::Error) -> Self {
+        ConfigReadError::Toml(err)
+    }
+}
+
+pub fn read_config(path: &str) -> Result<Config, ConfigReadError> {
+    let content = std::fs::read_to_string(path)?;
+    let config: Config = toml::from_str(&content)?;
+    Ok(config)
 }
