f65ffb60b53581ae5b337f8643a20e31c24b7f7d
0xcompose <0xcompose@maat.finance>
2025-11-15 22:36:20 +0300

    fea(types::stargate_api): generate Stargate API types and implement common methods to get data

---
+++ src/types/stargate_api/adapters.rs
@@ -0,0 +1,34 @@
+use crate::types::common::{Entry, EntryId, ResponseData};
+use crate::types::stargate_api::types::{Chain, StargateChainsResponseData};
+
+impl ResponseData for StargateChainsResponseData {
+    type Entry = Chain;
+
+    fn entries(&self) -> Vec<Self::Entry> {
+        self.chains.clone()
+    }
+}
+
+impl Entry for Chain {
+    fn id(&self) -> EntryId {
+        self.chain_key.clone()
+    }
+
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn format(&self) -> String {
+        format!(
+            "📛 Name: {}\n\
+             🔗 Chain ID: {}\n\
+             💰 Native Currency: {} ({})\n\
+             🔢 Decimals: {}",
+            self.name,
+            self.chain_id,
+            self.native_currency.symbol,
+            self.native_currency.name,
+            self.native_currency.decimals
+        )
+    }
+}
