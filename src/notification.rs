d4fd12453672cbcc0fb3d70fd40f2a7507b07044
0xcompose <0xcompose@maat.finance>
2025-11-22 12:44:35 +0300

    refactor(notify_on_change): move out to separate module

---
+++ src/notification.rs
@@ -0,0 +1,24 @@
+use log::{error, info};
+
+use crate::{clients::common::Entry, config::Config, telegram::send_message};
+
+pub fn notify_on_change<T: Entry>(
+    config: &Config,
+    entry: &T,
+    msg_header: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    info!(
+        "Notifying on change for entry: {} ({})",
+        entry.name(),
+        entry.id()
+    );
+
+    let message = format!("⚙️ {}\n\n{}\n\n{}", config.name, msg_header, entry.format());
+
+    if let Err(e) = send_message(&message) {
+        error!("Failed to send Telegram notification: {}", e);
+        return Err(e);
+    }
+
+    Ok(())
+}
